9f4fc9ac2a80d334c25b779fca2cbaae2e60651d
cterkesli <485604@student.fontys.nl>
2022-09-13 15:16:04 +0200

    Add project files.

---
+++ qwdasd/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace qwdasd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! ++++++++");
+        }
+    }
+}
